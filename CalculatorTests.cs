f9697f1832be3043964a2c9bd6052204442df0ba
Ivy Guo <gj1002@gmail.com>
2019-12-06 12:21:28 +1300

    first commit

---
+++ CalculatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Library.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Divide_PositiveNumbers_ReturnsPositiveQuotient()
+        {
+            //Arrange
+            int expected = 5;
+            int numerator = 20;
+            int denominator = 4;
+
+            //Act
+            int actual = Calculator.Divide(numerator, denominator);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_NegativeNumbers_ReturnsPositiveQuotient()
+        {
+            //Arrange
+            int expected = 5;
+            int numerator = -20;
+            int denominator = -4;
+
+            //Act
+            int actual = Calculator.Divide(numerator, denominator);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_PositiveNumeratorAndNegativeDenomitor_ReturnsNegativeQuotient()
+        {
+            //Arrange
+            int expected = -5;
+            int numerator = 20;
+            int denominator = -4;
+
+            //Act
+            int actual = Calculator.Divide(numerator, denominator);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
